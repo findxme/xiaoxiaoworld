3643da444b2cfa44a340f702cd9486e0f3d19fb6
Pr1p <244877949@qq.com>
2019-09-29 04:03:55 +0800

    添加Reader Controller/Service/ServiceImpl

---
+++ src/main/java/com/xmx/ssm/service/TReadersService.java
@@ -0,0 +1,24 @@
+package com.xmx.ssm.service;
+
+
+import com.xmx.ssm.entity.TReader;
+
+import java.util.List;
+import java.util.Map;
+
+public interface TReadersService {
+    /*查找全部*/
+    List<Map<String ,Object>> findReadersAll();
+
+    /*删除读者信息*/
+    void deleteReadersNo(TReader tReader);
+
+    /*增加读者信息*/
+    void insertReader(TReader tReader);
+
+    /*更新读者信息*/
+    void  updateReader(TReader tReader);
+
+    /*条件查询*/
+    List<TReader> findReadersOne(TReader tReader);
+}
